aae2235fa493c04e69bccfda9755bd56588675ab
weilbith <thore@weilbier.net>
2018-01-03 08:58:30 +0100

    Initial commit with the basic functionality.

---
+++ webpack.config.js
@@ -0,0 +1,59 @@
+const webpack = require('webpack');
+const merge = require('webpack-merge');
+const path = require('path');
+
+var config = {
+  output: {
+    path: path.resolve(__dirname + '/dist/'),
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader',
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.css$/,
+        loader: 'style!less!css'
+      }
+    ]
+  },
+  externals: {
+    moment: 'moment'
+  },
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin( {
+      minimize : true,
+      sourceMap : false,
+      mangle: true,
+      compress: {
+        warnings: false
+      }
+    } )
+  ]
+};
+
+
+module.exports = [
+  merge(config, {
+    entry: path.resolve(__dirname + '/src/plugin.js'),
+    output: {
+      filename: 'vue-toggle.min.js',
+      libraryTarget: 'window',
+      library: 'VueToggle',
+    }
+  }),
+  merge(config, {
+    entry: path.resolve(__dirname + '/src/Toggle.vue'),
+    output: {
+      filename: 'vue-toggle.js',
+      libraryTarget: 'umd',
+      library: 'vue-toggle',
+      umdNamedDefine: true
+    }
+  })
+];
